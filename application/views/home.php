056ff81782e0bf75025a63e3b17f58e82e05845f
yanuarbeszt <yanuar.ridwan.h@gmail.com>
2021-01-22 17:17:32 +0700

    [#] initial commit

---
+++ application/views/home.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Renomenu | Don't Worry To Be Hungry!</title>
+	<link rel="icon" href="<?php base_url(); ?>assets/images/favicon.png">
+
+	<link rel="stylesheet" href="<?php base_url(); ?>assets/css/bootstrap.css">
+	<link rel="stylesheet" href="<?php base_url(); ?>assets/css/bootstrap-grid.css">
+	<link rel="stylesheet" href="<?php base_url(); ?>assets/css/style.css">
+
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+	<script src="<?php base_url() ?>assets/js/bootstrap.js"></script>
+	<script src="https://kit.fontawesome.com/508d612a62.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+	<header>
+		<div class="container">
+			<div class="logo">
+				<a href="">
+					<img src="<?php base_url(); ?>assets/images/renomenu-logo-21.png" alt="logo-renomenu">
+				</a>
+			</div>
+			<div class="menu">
+				<ul>
+					<li>
+						<a href="">Beranda</a>
+					</li>
+					<li>
+						<a href="">Pusat Bantuan</a>
+					</li>
+					<li>
+						<a href="">Blog</a>
+					</li>
+					<li>
+						<a href="">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</header>
+	<section class="banner">
+		<div class="container">
+			<div class="text-banner">
+				<h3>Aplikasi Menu Elektronik, <br>
+					Order dengan pembayaran tanpa terpaku <br>
+					pada aplikasi tertentu dalam satu perangkat</h3>
+
+				<a href="">
+					<img src="<?php base_url() ?>assets/images/google-play.png" alt="download android app">
+				</a>
+			</div>
+			<div class="image-banner">
+				<img src="<?php base_url() ?>assets/images/banner2.png" alt="banner-image1">
+				<img src="<?php base_url() ?>assets/images/coffee@3x.png" alt="banner-image2">
+				<img src="<?php base_url() ?>assets/images/fries@3x.png" alt="banner-image3">
+				<img src="<?php base_url() ?>assets/images/boxfastfood@3x.png" alt="banner-image4">
+				<img src="<?php base_url() ?>assets/images/soda@3x.png" alt="banner-image5">
+				<img src="<?php base_url() ?>assets/images/sandwich@3x.png" alt="banner-image6">
+				<img src="<?php base_url() ?>assets/images/rendang.png" alt="banner-image7">
+				<img src="<?php base_url() ?>assets/images/martabakmanis.png" alt="banner-image8">
+				<img src="<?php base_url() ?>assets/images/head-08.png" alt="banner-image9">
+				<img src="<?php base_url() ?>assets/images/head-07.png" alt="banner-image10">
+				<img src="<?php base_url() ?>assets/images/head-04.png" alt="banner-image11">
+				<img src="<?php base_url() ?>assets/images/head-03.png" alt="banner-image12">
+			</div>
+		</div>
+	</section>
+	<section class="event-feature">
+		<div class="container">
+			<div class="event-carousel">
+				<img src="<?php base_url(); ?>assets/images/banner-event.png" alt="banner-event">
+			</div>
+			<div class="feature">
+				<div class="element-feature">
+					<img src="<?php base_url(); ?>assets/images/delivery button.png" alt="delivery button">
+				</div>
+				<div class="element-feature">
+					<img src="<?php base_url(); ?>assets/images/dine in button.png" alt="dine in button">
+				</div>
+				<div class="element-feature">
+					<img src="<?php base_url(); ?>assets/images/takeAway button.png" alt="takeAway button">
+				</div>
+				<div class="element-feature">
+					<img src="<?php base_url(); ?>assets/images/qr button.png" alt="qr button">
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="blog">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-7">
+					<a href="">
+						<img src="<?php base_url(); ?>assets/images/banner-help.png" alt="banner-help">
+					</a>
+				</div>
+				<div class="col-md-5">
+					<a href="">
+						<div class="blog">
+							<div class="img-blog">
+								<img src="<?php base_url(); ?>assets/images/restauran.jpeg" alt="restauran">
+							</div>
+							<div class="desc-blog">
+								<h6>10 restauran mewah di jakarta</h6>
+								<div class="athor-blog">
+									<span>admin</span>
+								</div>
+								<div class="date-blog">
+									<span>20 Januari 2021</span>
+								</div>
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<a href="">
+						<img src="<?php base_url(); ?>assets/images/renomenu-logo-21.png" alt="logo-renomenu">
+					</a>
+					<span>Aplikasi Menu Elektronik, Order dengan <br>
+						pembayaran tanpa terpaku pada aplikasi <br>
+						tertentu dalam satu perangkat.</span>
+					<a href="">
+						<img src="<?php base_url() ?>assets/images/google-play.png" alt="download android app">
+					</a>
+					<a href=""><i class="fab fa-facebook"></i></a>
+					<a href=""><i class="fab fa-instagram"></i></a>
+					<a href=""><i class="fab fa-youtube"></i></a>
+				</div>
+				<div class="col-md-4">
+					<div class="row">
+						<i class="fas fa-map-marker-alt"></i>
+						<span><b>PT ACE GLOBAL CONSULTING AND <br>
+								INTEGRATION (AGCI)</b> Menara Indomaret,<br>
+							Jl Boulevard Pantai Indah Kapuk - Lantai 15<br>
+							Kel. Kamal Utara, Kec. Penjaringan<br>
+							Jakarta Utara, DKI Jakarta 14470 <b>(New Office)</b>
+						</span>
+					</div>
+					<div class="row">
+						<i class="fas fa-map-marker-alt"></i>
+						<span>
+							<b>Gedung Indomaret</b>, Jl. Terusan Angkasa <br>
+							Blok B2 Kav.1 – 7th floor Gunung Sahari<br>
+							Selatan – Kemayoran Jakarta Pusat 10610<br>
+							<b>(Previous Office)</b>
+						</span>
+					</div>
+					<div class="row">
+						<i class="fas fa-phone-alt"></i>
+						<a href=""><span>(021)2158-4028</span></a>
+					</div>
+					<div class="row">
+						<i class="fas fa-paper-plane"></i>
+						<a href=""><span>sales@renotech.co.id</span></a>
+					</div>
+					<div class="row">
+						<i class="fab fa-whatsapp"></i>
+						<a href=""><span>(+62)821-8900-8890 (Click to open Whatsapp)</span></a>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<iframe class="embed-responsive-item" allowfullscreen="allowfullscreen" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.805071613025!2d106.84786951450177!3d-6.156856162059117!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f576ed9e5d79%3A0xfa24f66ccc8f8e89!2sRenotech+(PT.+Ace+Global+Consulting+%26+Integration)!5e0!3m2!1sen!2sid!4v1540440188935" width="550"></iframe>yJTNBbnVsbCUyQyUyMm1hcmtlcnMlMjIlM0ElNUIlNUQlN0Q="></iframe>
+					<!-- <div id="map"></div> -->
+				</div>
+			</div>
+		</div>
+
+	</footer>
+
+
+	<!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBIwzALxUPNbatRBj3Xi1Uhp0fFzwWNBkE&callback=initMap&libraries=&v=weekly" defer></script>
+	<script>
+		// Initialize and add the map
+		function initMap() {
+			// The location of Uluru
+			const uluru = {
+				lat: -25.344,
+				lng: 131.036
+			};
+			// The map, centered at Uluru
+			const map = new google.maps.Map(document.getElementById("map"), {
+				zoom: 4,
+				center: uluru,
+			});
+			// The marker, positioned at Uluru
+			const marker = new google.maps.Marker({
+				position: uluru,
+				map: map,
+			});
+		}
+	</script> -->
+</body>
+
+</html>
